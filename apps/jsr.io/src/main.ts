01060b6b8f36f59b7276a46f547d15e23986e009
Eliseu Videira <eliseu@tryfinch.com>
2025-05-21 10:48:25 -0300

    fix: ci

--- apps/jsr.io/src/main.ts
+++ apps/jsr.io/src/main.ts
@@ -1,7 +1,10 @@
-import { http } from "@scrappers/http-client";
+import { HttpClient } from "@scrappers/http-client";
 
 const main = async () => {
-  const response = await http.request("https://jsr.io/@oak/oak", {
+  const http = HttpClient("https://jsr.io");
+  const response = await http.request({
+    method: "GET",
+    path: "/@oak/oak",
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
