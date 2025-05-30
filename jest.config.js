56fdb6426d628d9206d96a1e47b9f0692aa12457
Eliseu Videira <eliseu@tryfinch.com>
2025-05-30 09:35:16 -0300

    fix: ci

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
-const { createDefaultPreset } = require("ts-jest");
+const { createDefaultPreset } = require('ts-jest');
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
     ...tsJestTransformCfg,
   },
+  testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/out/'],
 };
